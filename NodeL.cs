c362a9d6201fc137281386385701120aa182d6c4
Bartek Prawdzik <157278304+bartexpl11@users.noreply.github.com>
2024-11-17 16:06:23 +0100

    Add files via upload

---
+++ NodeL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class NodeL
+    {
+        public NodeL n;
+        public NodeL p;
+        public int data = 3;
+        public NodeL(int liczba)
+        {
+            this.data = liczba;
+        }
+    }
+}
